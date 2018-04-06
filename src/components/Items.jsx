9a30673750c4fe973ae3eedea42ace9cfdf28a87
Soham Bhattacharjee <soham.onlyn@gmail.com>
2018-04-06 14:20:52 -0700

    table for items

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const Items = (props) => {
-    return <div>Items {props.match.params.itemid}</div>
+class Items extends Component {
+    constructor() {
+        super()
+        this.state = {
+            itemDetails: []
+        }
+    }
+    componentWillReceiveProps(nextProp) {
+        axios.get(`https://davids-restaurant.herokuapp.com/menu_items.json?category=${nextProp.match.params.itemid}`).then(res => {
+            this.setState({ itemDetails: res.data.menu_items })
+        })
+    }
+    render() {
+        return (<div><h4>Items in Category: ({this.props.match.params.itemid})</h4>
+            {this.state.itemDetails.length > 0 &&
+                <table style={{border: '1px solid gray'}}>
+                    <thead>
+                        <tr><th>Name</th><th>Description</th></tr>
+                    </thead>
+                    <tbody>
+                        {this.state.itemDetails.map(item => {
+                            return <tr><td>{item.name}</td><td>{item.description}</td></tr>
+                        })}
+                    </tbody>
+                </table>
+            }
+        </div>)
+    }
 }
 
 export default Items
